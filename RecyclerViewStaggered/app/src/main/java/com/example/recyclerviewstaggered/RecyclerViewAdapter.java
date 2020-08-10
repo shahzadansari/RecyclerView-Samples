67616bc6555b5d60c34162dfc6f06d8605e8baa2
Shahzad Ansari <shahzadansari03@outlook.com>
2020-08-10 15:41:56 +0500

    Created RecyclerViewAdapter.java

---
+++ RecyclerViewStaggered/app/src/main/java/com/example/recyclerviewstaggered/RecyclerViewAdapter.java
@@ -0,0 +1,70 @@
+package com.example.recyclerviewstaggered;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private Context mContext;
+    private ArrayList<String> mImages;
+    private ArrayList<String> mImageNames;
+
+    public RecyclerViewAdapter(Context mContext, ArrayList<String> mImages, ArrayList<String> mImageNames) {
+        this.mContext = mContext;
+        this.mImages = mImages;
+        this.mImageNames = mImageNames;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.layout_grid_item, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        RequestOptions requestOptions = new RequestOptions()
+                .placeholder(R.drawable.ic_launcher_background);
+
+        Glide.with(mContext)
+                .load(mImages.get(position))
+                .apply(requestOptions)
+                .into(holder.image);
+
+        holder.image_description.setText(mImageNames.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImages.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView image;
+        TextView image_description;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.image);
+            image_description = itemView.findViewById(R.id.image_description);
+        }
+    }
+
+}
